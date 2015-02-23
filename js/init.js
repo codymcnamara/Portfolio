27ce7891037801bdc0c560b207c0575cb182851d
Cody McNamara <codyjamesmcnamara@gmail.com>
2015-02-23 12:19:13 -0800

    changed description and title

--- js/init.js
+++ js/init.js
@@ -9,7 +9,7 @@
 	var settings = {
 
 		// Parallax background effect?
-			parallax: true,
+			// parallax: true,
 
 		// Parallax factor (lower = more intense, higher = less intense).
 			parallaxFactor: 20
